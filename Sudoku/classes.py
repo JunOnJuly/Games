5110945a474886cef92585dd0031bfa76b70a279
JunOnJuly <wlgnstjd0717@gmail.com>
2022-04-13 19:57:17 +0900

    init add sudoku

--- Sudoku/classes.py
+++ Sudoku/classes.py
@@ -1,3 +1,7 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
 class RangeError(Exception):
     def __str__(self):
         return "올바른 범위의 수를 입력해 주세요"
@@ -6,3 +10,17 @@ class RangeError(Exception):
 class NonNumberError(Exception):
     def __str__(self):
         return "숫자를 입력해 주세요"
+
+
+class Sudoku(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Sudoku')
+        self.setWindowIcon(QIcon('rabbit.jpg'))
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
