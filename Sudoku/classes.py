0cb7c002c7ffcc4f680364bbfe0c576e2d586252
JunOnJuly <wlgnstjd0717@gmail.com>
2022-04-16 01:04:06 +0900

    delete .py

--- Sudoku/classes.py
+++
@@ -1,9 +0,0 @@
-class RangeError(Exception):
-    def __str__(self):
-        return "올바른 범위의 수를 입력해 주세요"
-
-
-class NonNumberError(Exception):
-    def __str__(self):
-        return "숫자를 입력해 주세요"
-
