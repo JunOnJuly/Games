4dc00f36af7b8c982265b70f4c1a75b1b77599dd
JunOnJuly <wlgnstjd0717@gmail.com>
2022-04-18 01:51:35 +0900

    replace method

--- games/src/store/index.js
+++ games/src/store/index.js
@@ -51,7 +51,7 @@ export default new Vuex.Store({
                         baseMap[selectedIdxColumn[1]][column] = tempNum
                     }
                 }
-                count += 1
+                count = count + 1
             }
             return baseMap
         }
