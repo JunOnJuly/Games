bce0913713f536ea9198c3ac2df91b8334b930ea
JunOnJuly <wlgnstjd0717@gmail.com>
2022-05-16 01:20:29 +0900

    add methods

--- games/src/components/SudokuComp.vue
+++ games/src/components/SudokuComp.vue
@@ -3,8 +3,19 @@
 </template>
 
 <script>
+
+import _ from 'lodash'
+
 export default {
-  
+  methods: {
+    selectIdx (areaNum) {
+      const candidList = _.range(areaNum * 3, areaNum * 3 + 2)
+      const idxInArea1 = _.samplesize(candidList, 2)
+
+      return idxInArea1
+    },
+    placeNumIntoMap ()
+  }
 }
 </script>
 
